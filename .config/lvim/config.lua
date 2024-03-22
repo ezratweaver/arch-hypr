223ad52aa0e10f7936aadae1bde9512ccf20a0be
ezratweaver <ezratweaver@gmail.com>
2024-03-22 19:06:49 -0400

    Update 2

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -14,36 +14,36 @@ lvim.plugins =
   { 'navarasu/onedark.nvim' },
   { 'abecodes/tabout.nvim' },
   { 'xiyaowong/transparent.nvim' },
+  { 'miikanissi/modus-themes.nvim' },
+  { 'folke/lsp-colors.nvim' },
 }
 
 
-lvim.colorscheme = "onedark" -- Set OneDark to the default theme
+lvim.colorscheme = "modus" -- Set OneDark to the default theme
 
 -- lvim.transparent_window = true
 
 lvim.builtin.which_key.mappings["ss"] = {
   "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Single file search" }
-
-require('tabout').setup {
-    tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
-    backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
-    act_as_tab = true, -- shift content if tab out is not possible
-    act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-    default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-    default_shift_tab = '<C-d>', -- reverse shift default action,
-    enable_backwards = true, -- well ...
-    completion = true, -- if the tabkey is used in a completion pum
-    tabouts = {
-      {open = "'", close = "'"},
-      {open = '"', close = '"'},
-      {open = '`', close = '`'},
-      {open = '(', close = ')'},
-      {open = '[', close = ']'},
-      {open = '{', close = '}'}
-    },
-    ignore_beginning = false, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-    exclude = {} -- tabout will ignore these filetypes
-}
+-- require('tabout').setup { tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+--     backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+--     act_as_tab = true, -- shift content if tab out is not possible
+--     act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+--     default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+--     default_shift_tab = '<C-d>', -- reverse shift default action,
+--     enable_backwards = true, -- well ...
+--     completion = true, -- if the tabkey is used in a completion pum
+--     tabouts = {
+--       {open = "'", close = "'"},
+--       {open = '"', close = '"'},
+--       {open = '`', close = '`'},
+--       {open = '(', close = ')'},
+--       {open = '[', close = ']'},
+--       {open = '{', close = '}'}
+--     },
+--     ignore_beginning = false, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+--     exclude = { ".html" } -- tabout will ignore these filetypes
+-- }
 
 
 lvim.builtin.which_key.mappings["t"] = {
@@ -87,4 +87,14 @@ require('transparent').setup({
 })
 
 require('transparent').clear_prefix('NeoTree')
+
+vim.cmd("imap <C-c> <Esc>")
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- delay update diagnostics
+    update_in_insert = true,
+  }
+)
+
 
